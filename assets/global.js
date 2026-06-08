57ef30b18ef01b738d65724d4ee531566669aea7
jhonrinconlivocol <jhonrincon.livo@gmail.com>
2026-06-08 11:07:44 -0500

    Update global.js

--- assets/global.js
+++ assets/global.js
@@ -1,5 +1,32 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-      console.log('Livo Premium Theme Loaded');
+const bundleOptions = document.querySelectorAll(
+'.lp-bundle-option input[type="radio"]'
+);
 
-      });
+const hiddenVariantInput = document.querySelector(
+'form[action="/cart/add"] input[name="id"]'
+);
+
+bundleOptions.forEach(option => {
+
+```
+option.addEventListener('change', () => {
+
+  const variantId =
+    option.dataset.variantId;
+
+  if (
+    hiddenVariantInput &&
+    variantId
+  ) {
+    hiddenVariantInput.value =
+      variantId;
+  }
+
+});
+```
+
+});
+
+});
