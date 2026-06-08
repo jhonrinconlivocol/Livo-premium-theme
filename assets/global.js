844e60b37ed40fefa7824ddfe0d39ed05cb0fc1c
jhonrinconlivocol <jhonrincon.livo@gmail.com>
2026-06-08 11:09:31 -0500

    Update global.js

--- assets/global.js
+++ assets/global.js
@@ -8,6 +8,22 @@ const hiddenVariantInput = document.querySelector(
 'form[action="/cart/add"] input[name="id"]'
 );
 
+const currentPrice = document.querySelector(
+'[data-current-price]'
+);
+
+const comparePrice = document.querySelector(
+'[data-compare-price]'
+);
+
+const savePrice = document.querySelector(
+'[data-price-save]'
+);
+
+const dailyPrice = document.querySelector(
+'[data-daily-price]'
+);
+
 bundleOptions.forEach(option => {
 
 ```
@@ -16,12 +32,55 @@ option.addEventListener('change', () => {
   const variantId =
     option.dataset.variantId;
 
+  const price =
+    parseInt(option.dataset.variantPrice || 0);
+
+  const compare =
+    parseInt(option.dataset.variantCompare || 0);
+
   if (
     hiddenVariantInput &&
     variantId
   ) {
     hiddenVariantInput.value =
       variantId;
+  }
+
+  if (currentPrice) {
+
+    currentPrice.textContent =
+      Shopify.formatMoney(price);
+  }
+
+  if (
+    comparePrice &&
+    compare > price
+  ) {
+
+    comparePrice.textContent =
+      Shopify.formatMoney(compare);
+  }
+
+  if (
+    savePrice &&
+    compare > price
+  ) {
+
+    savePrice.textContent =
+      'Ahorra ' +
+      Shopify.formatMoney(
+        compare - price
+      );
+  }
+
+  if (dailyPrice) {
+
+    dailyPrice.textContent =
+      'Menos de ' +
+      Shopify.formatMoney(
+        Math.round(price / 30)
+      ) +
+      ' al día';
   }
 
 });
